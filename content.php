ae7aa5ec9a5753d7b3663246ee74d967570adef5
José Solorzano <solorzano202009@gmail.com>
2019-06-11 17:07:05 -0400

    Nuevo archivo de configuración.

--- content.php
+++ content.php
@@ -22,6 +22,7 @@
               <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-bar bar1"></span><span class="navbar-toggler-bar bar2"></span><span class="navbar-toggler-bar bar3"></span></button>
             </div>
             <div class="collapse navbar-collapse justify-content-end" id="navigation">
+			  <?php if($config['config']['menu']['active'] == 1){ ?>
               <ul class="navbar-nav">
 				<?php foreach($config['menu'] as $key_m => $menu){?>
 					<?php if($key_m != 'language'){ ?>
@@ -48,6 +49,7 @@
 					<?php } ?>
 				<?php } ?>
               </ul>
+              <?php } ?>
             </div>
           </div>
         </nav>
@@ -467,6 +469,7 @@
   </div>
 </div></div>
     </div>
+    <?php if($config['config']['footer']['active'] == 1){ ?>
     <footer class="footer">
       <div class="container text-center"><a class="cc-facebook btn btn-link" href="https://www.facebook.com/jose.solorzano.9849"><i class="fa fa-facebook fa-2x " aria-hidden="true"></i></a><a class="cc-twitter btn btn-link " href="https://twitter.com/jsolorzano18"><i class="fa fa-twitter fa-2x " aria-hidden="true"></i></a><a class="cc-linkedin btn btn-link" href="https://www.linkedin.com/in/jose-solorzano-4307b372/"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a><a class="cc-github btn btn-link" href="https://github.com/jsolorzano"><i class="fa fa-github fa-2x " aria-hidden="true"></i></a></div>
       <div class="h4 title text-center">José Solorzano</div>
@@ -474,6 +477,7 @@
         <p>&copy; Creative CV. All rights reserved.<br>Design - <a class="credit" href="https://templateflip.com" target="_blank">TemplateFlip</a></p>
       </div>
     </footer>
+    <?php } ?>
     <script src="js/core/jquery.3.2.1.min.js"></script>
     <script src="js/core/popper.min.js"></script>
     <script src="js/core/bootstrap.min.js"></script>
---
+++ php/contents/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Variables de activación/desactivación del menú y el footer. Valores posibles 0 o 1.
+
+$config['config']['menu']['active'] = 1;
+
+$config['config']['footer']['active'] = 1;
+
+?>
